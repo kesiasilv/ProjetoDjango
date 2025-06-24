ffc6d4be9687e330d8ff1bcc2e5b4b153caa1378
Késia Silva <kesiasilvab6@gmail.com>
2025-06-24 19:23:18 -0300

    Update urls.py

--- Projeto Django/todolist/todolist/urls.py
+++ Projeto Django/todolist/todolist/urls.py
@@ -3,5 +3,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('tasks.urls')),  # Agora tudo que for '', 'add/', 'edit/22/', etc, vem de tasks.urls
+    path('', include('tasks.urls')),  
 ]
