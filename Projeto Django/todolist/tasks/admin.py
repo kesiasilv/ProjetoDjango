8e7363990619f0883131062b891fdff485a16e03
Késia Silva <kesiasilvab6@gmail.com>
2025-06-24 19:24:11 -0300

    Update admin.py

--- Projeto Django/todolist/tasks/admin.py
+++ Projeto Django/todolist/tasks/admin.py
@@ -3,6 +3,6 @@ from .models import Task
 
 @admin.register(Task)
 class TaskAdmin(admin.ModelAdmin):
-    list_display = ('id', 'title', 'completed', 'created_at')  # Campos que você quer que apareçam na listagem
-    list_filter = ('completed',)  # Filtros laterais
-    search_fields = ('title',)  # Campo de busca
+    list_display = ('id', 'title', 'completed', 'created_at')  
+    list_filter = ('completed',) 
+    search_fields = ('title',)  
